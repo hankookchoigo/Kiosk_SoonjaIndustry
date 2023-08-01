d0e11490f05461b3a95cd21b1068c76b332d65e0
hankookchoigo <ryan.sg.shim@gmail.com>
2023-08-01 20:52:54 +0900

    First main Branch commit(incomplete)

---
+++ src/Launching/Kiosk_Launcher.java
@@ -0,0 +1,9 @@
+package Launching;
+
+public class Kiosk_Launcher {
+    // *중요* mySQL 데이터베이스가 실행되었는지 확인하시오 *중요*
+    // *Caution* Check whether mySQL database is launched. *Caution*
+    public static void main(String[] args) {
+        new Modeselect(0);
+    }
+}
